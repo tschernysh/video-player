848280be73968799fcdb82130f109b217a9727a5
tschernysh <tschernysh@gmail.com>
2021-10-07 19:04:23 +0300

    progress bar

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ let toggle = document.getElementById('toggle')
 let play = document.getElementsByClassName('play')
 let pause = document.getElementsByClassName('pause')
 let current = document.getElementById('currentTime')
+let duration = document.getElementById('durationTime')
+let progress = document.getElementById('progress')
 
-console.log(video);
 
 const updateVideoStatus = () => {
-    console.log(video.currentTime);
     if(video.paused){
         pause[0].classList.remove('current')
         play[0].classList.add('current')
@@ -17,11 +17,15 @@ const updateVideoStatus = () => {
     }
 }
 
+const updateProgress = (currentTime, durationTime) => {
+    progress.style.width = !currentTime ? currentTime : ((currentTime * 100 ) / durationTime) + '%'
+    
+}
 
 const timeFormatter = (seconds) => {
+    seconds = Math.floor(seconds)
     let full = Math.floor(seconds/60)
     let secs = seconds%60
-    
     if(secs < 10){
         secs = '0' + secs
     }
@@ -29,12 +33,16 @@ const timeFormatter = (seconds) => {
     return `${full}:${secs}`
 }
 
+const setDuration = () => {
+    duration.innerText = timeFormatter(video.duration)
+}
 
 const updateCurrentTime = () => {
     current.innerText = timeFormatter(Math.floor(video.currentTime))
 }
 
 setInterval( () => !video.paused && updateCurrentTime(), 1000)
+setInterval( () => !video.paused && updateProgress(video.currentTime, video.duration), 200)
 
 const toggleVideo = async () => {
     if(video.paused){
@@ -47,17 +55,21 @@ const toggleVideo = async () => {
 toggle.addEventListener('click', toggleVideo)
 video.addEventListener('click', toggleVideo)
 document.addEventListener('keydown', e => {
-    console.log(e);
     if(e.code === 'Space'){
         toggleVideo()
     }else if(e.code === 'ArrowRight'){
         video.currentTime = Math.floor(video.currentTime + 5)
         updateCurrentTime()
+        updateProgress(video.currentTime, video.duration)
     }else if(e.code === 'ArrowLeft'){
         video.currentTime = Math.floor(video.currentTime - 5)
         updateCurrentTime()
+        updateProgress(video.currentTime, video.duration)
     }
 })
 video.addEventListener( 'play' , updateVideoStatus)
 video.addEventListener( 'pause' , updateVideoStatus)
-video.addEventListener( 'loadeddata' , updateVideoStatus)
+video.addEventListener( 'loadeddata' , () => {
+    updateVideoStatus()
+    setDuration()
+})
