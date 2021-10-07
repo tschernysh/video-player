a21db68df74b5bafe8753bcbc2ccfe81cddd6ad4
tschernysh <tschernysh@gmail.com>
2021-10-07 12:58:48 +0300

    current time

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ let video = document.getElementById('video')
 let toggle = document.getElementById('toggle')
 let play = document.getElementsByClassName('play')
 let pause = document.getElementsByClassName('pause')
+let current = document.getElementById('currentTime')
 
 
 const updateVideoStatus = () => {
+    console.log(video.currentTime);
     if(video.paused){
         pause[0].classList.remove('current')
         play[0].classList.add('current')
@@ -15,6 +17,23 @@ const updateVideoStatus = () => {
 }
 
 
+const timeFormatter = (seconds) => {
+    let full = Math.floor(seconds/60)
+    let secs = seconds%60
+    
+    if(secs < 10){
+        secs = '0' + secs
+    }
+
+    return `${full}:${secs}`
+}
+
+
+const updateCurrentTime = () => {
+    current.innerText = timeFormatter(Math.floor(video.currentTime))
+}
+
+setInterval( () => !video.paused && updateCurrentTime(), 1000)
 
 const toggleVideo = async () => {
     if(video.paused){
